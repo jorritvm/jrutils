625b2632aa80cb852bb049ff315a29054a67b6e8
jorritvm <jorrit.vm@gmail.com>
2020-09-19 16:52:55 +0200

    prep for v0.3.0 source package

--- R/lib_other.R
+++ R/lib_other.R
@@ -4,11 +4,12 @@
 #' @import devtools
 update_jrutils_package = function() {
   devtools_is_installed = "devtools" %in% installed.packages()[,"Package"]
-  stop("devtools is required but is not installed yet")
   if (devtools_is_installed) {
     devtools::install_github("jorritvm/jrutils")
     # install.packages(choose.files(default = "O:/ESM/IPL/SMA/Market modelling/04. Tools/Other/sma_R_utils/versions/.", caption = "Select the source package to install", multi = FALSE, filters = Filters[c("gz")]), repos = NULL, type="source")  
-  } 
+  }  else {
+    stop("devtools is required but is not installed yet")
+  }
 }
 
 #' utility function to cat undefined number of arguments and end with trailing newline 
