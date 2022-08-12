ed586370ace712ce8175d13e888e22795a4d3d22
jorritvm <44178217+jorritvm@users.noreply.github.com>
2022-08-12 10:28:00 +0200

    Update lib_other.R
    close #27

--- R/lib_other.R
+++ R/lib_other.R
@@ -23,24 +23,6 @@ catn = function(...) {
 }
 
 
-#' provided a vector of string dates in the standard year 2018 it will return a
-#' vector of timeids
-#'
-#' @param datetime string in the format ymd hm
-#'
-#' @return
-#' @export
-convert_datetime_to_timeid = function(datetime) {
-  result = c()
-  for (dt in datetime) {
-    dt = ymd_hm(dt)
-    tid = as.duration(ymd("2018-01-01") %--% dt) / dhours(1) + 1
-    result = c(result, tid)
-  }
-  return(result)
-}
-
-
 #' utility function to cat undefined number of arguments and end with trailing newline 
 #' and leading timestamp, and starting tic()
 #' 
